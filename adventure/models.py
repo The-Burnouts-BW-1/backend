58dbe2a686217fc2ca00af8c0a075d5bb41f764e
taterntots <matthew.a.weidner@gmail.com>
2020-04-30 21:36:33 -0400

    uncommented some code we needed for traversal

--- adventure/models.py
+++ adventure/models.py
@@ -15,25 +15,26 @@ class Room(models.Model):
     w_to = models.IntegerField(default=0)
     x = models.IntegerField(default=0)
     y = models.IntegerField(default=0)
-    # def connectRooms(self, destinationRoom, direction):
-    #     destinationRoomID = destinationRoom.id
-    #     try:
-    #         destinationRoom = Room.objects.get(id=destinationRoomID)
-    #     except Room.DoesNotExist:
-    #         print("That room does not exist")
-    #     else:
-    #         if direction == "n":
-    #             self.n_to = destinationRoomID
-    #         elif direction == "s":
-    #             self.s_to = destinationRoomID
-    #         elif direction == "e":
-    #             self.e_to = destinationRoomID
-    #         elif direction == "w":
-    #             self.w_to = destinationRoomID
-    #         else:
-    #             print("Invalid direction")
-    #             return
-    #         self.save()
+    
+    def connectRooms(self, destinationRoom, direction):
+        destinationRoomID = destinationRoom.id
+        try:
+            destinationRoom = Room.objects.get(id=destinationRoomID)
+        except Room.DoesNotExist:
+            print("That room does not exist")
+        else:
+            if direction == "n":
+                self.n_to = destinationRoomID
+            elif direction == "s":
+                self.s_to = destinationRoomID
+            elif direction == "e":
+                self.e_to = destinationRoomID
+            elif direction == "w":
+                self.w_to = destinationRoomID
+            else:
+                print("Invalid direction")
+                return
+            self.save()
 
     def connect_rooms(self, connecting_room, direction):
         '''
